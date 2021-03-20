f68d0889242900c6e3b5e385a77459c090d3d426
Aishwarya Upadhyaya <ashyu136@gmail.com>
2021-03-21 00:15:19 +0530

    Activities Created

---
+++ app/src/main/java/com/aishwarya/weighttracker/BackUpSettingActivity.java
@@ -0,0 +1,14 @@
+package com.aishwarya.weighttracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BackUpSettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_back_up_setting);
+    }
+}
